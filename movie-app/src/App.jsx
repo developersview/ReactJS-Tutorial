88b5979660afc87d344d824d6fdaa3fa9e9896d6
developersview <pcslg1998@gmail.com>
2023-06-10 00:34:18 +0530

    fixing https issue

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import SearchIcon from './assets/search.svg';
 import MovieCard from './MovieCard';
 
-const API_URL = "http://www.omdbapi.com?apikey=962bcb36";
+const API_URL = "https://www.omdbapi.com?apikey=962bcb36";
 
 const App = () => {
 
