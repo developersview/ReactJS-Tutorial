4f302230889d6c65193bba87aa436c622a906212
developersview <pcslg1998@gmail.com>
2023-06-16 03:07:42 +0530

    react hooks - useState and event handling

--- textutils/src/App.jsx
+++ textutils/src/App.jsx
@@ -1,9 +1,15 @@
 import NavBar from "./Components/NavBar";
+import TextForm from "./Components/TextForm";
 
 function App() {
   return (
-    <NavBar title="TextUtils" aboutText="About" />
+    <>
+      <NavBar title="TextUtils" aboutText="About" />
+      <div className="container">
+        <TextForm heading="Enter text to analyze" />
+      </div>
 
+    </>
   );
 }
 
---
+++ textutils/src/Components/TextForm.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+const TextForm = (props) => {
+    const [text, setText] = useState('Enter text here');
+
+    const handleUpperClick = () => {
+        console.log("Uppercase was clicked!");
+        setText(text.toUpperCase());
+    }
+    const handleLowerClick = () => {
+        console.log("Lowercase was clicked!");
+        setText(text.toLowerCase());
+    }
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+
+    return (
+        <>
+            <h4>{props.heading}</h4>
+            <Form>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Control
+                        value={text}
+                        as="textarea"
+                        rows={10}
+                        onChange={handleOnChange} />
+                </Form.Group>
+                <Button variant="primary" size="md" onClick={handleUpperClick}>
+                    Convert to Uppercase
+                </Button>{' '}
+                <Button variant="warning" size="md" onClick={handleLowerClick}>
+                    Convert to Uppercase
+                </Button>
+            </Form>
+        </>
+    )
+}
+
+export default TextForm;
