4e59ad7723d562b0b51c1dbe6e52ac3e51b447de
developersview <pcslg1998@gmail.com>
2023-08-24 12:18:43 +0530

    chapter 2 : components, props, jsx

--- react-masterclass/src/App.jsx
+++ react-masterclass/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Video from './components/Video'
+import './App.css'
 
 const App = () => {
     return (
-        <>
+        <div className='App'>
             <h2>Components</h2>
-            <Video />
-        </>
+            <Video title='React JS Tutorial' channel='ISRO Official' views='4.5M' time='August 23, 2023 17:20PM' />
+            <Video title='Node JS Titorial' channel='Technical Guruji' views='12M' time='July 8th, 2023 08:45AM' />
+        </div>
     )
 }
 
--- react-masterclass/src/components/Video.jsx
+++ react-masterclass/src/components/Video.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
+import './Video.css'
 
-const Video = () => {
+const Video = ({ title, channel, views, time }) => {
+    let topic = 'Components, Props and JSX'
+    let bg = 'dark';
     return (
         <>
-            <img
-                src="https://placehold.co/300x200.png"
-                alt="Katherine Johnson"
-            />
-            <p>React JS Components </p>
+            <div className='container'>
+                <div className="pic">
+                    <img
+                        src="https://placehold.co/300x200.png"
+                        alt="Katherine Johnson"
+                    />
+                </div>
+                <div className="title">{title}</div>
+                <div className="channel">{channel}</div>
+                <div className="views">
+                    {views} Views <span>.</span> {time}
+                </div>
+            </div>
+
         </>
-
-
     )
 }
-
 
 export default Video
