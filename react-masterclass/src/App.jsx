63a3781267f458424e668d3bf643dec3a20c20ba
developersview <pcslg1998@gmail.com>
2023-08-24 12:22:31 +0530

    chapter 2 : components, props, jsx

--- react-masterclass/src/App.jsx
+++ react-masterclass/src/App.jsx
@@ -6,8 +6,24 @@ const App = () => {
     return (
         <div className='App'>
             <h2>Components</h2>
-            <Video title='React JS Tutorial' channel='ISRO Official' views='4.5M' time='August 23, 2023 17:20PM' />
-            <Video title='Node JS Titorial' channel='Technical Guruji' views='12M' time='July 8th, 2023 08:45AM' />
+            <Video
+                title='React JS Tutorial'
+                channel='ISRO Official'
+                views='4.5M'
+                time='1 day ago'
+            />
+            <Video
+                title='Node JS Titorial'
+                channel='Technical Guruji'
+                views='12M'
+                time='2 months ago'
+            />
+            <Video
+                title='Node JS Titorial'
+                channel='Technical Guruji'
+                views='12M'
+                time='2 months ago'
+            />
         </div>
     )
 }
