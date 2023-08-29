59a83ae3d3b4a745bbef5ffd1a627ffba40d1ef3
developersview <pcslg1998@gmail.com>
2023-08-30 03:29:48 +0530

    chapter 3: conditional rendering and map

---
+++ react-masterclass/src/data/data.js
@@ -0,0 +1,36 @@
+let videos = [
+    {
+        id: 1,
+        title: 'Chandrayaan - 3',
+        channel: 'ISRO Official',
+        views: '4.5M',
+        time: '1 day ago',
+        verified: true
+    },
+    {
+        id: 2,
+        title: 'Node JS Titorial',
+        channel: 'JavaScript Mastery',
+        views: '454K',
+        time: '2 day ago',
+        verified: false
+    },
+    {
+        id: 3,
+        title: 'Nothing Phone',
+        channel: 'Technical Guruji',
+        views: '5M',
+        time: '12 days ago',
+        verified: true
+    },
+    {
+        id: 4,
+        title: 'React JS Titorial',
+        channel: 'CodeEvolution',
+        views: '981K',
+        time: '1 day ago',
+        verified: false
+    }
+]
+
+export default videos;
