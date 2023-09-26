697f945535dc2f3553d1d54a6504053c65aa7d52
developersview <pcslg1998@gmail.com>
2023-09-27 00:52:26 +0530

    Chapter 6: React Forms

--- react-masterclass/src/components/AddVideo.jsx
+++ react-masterclass/src/components/AddVideo.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AddVideo.css'
 
 const AddVideo = () => {
 
-    const handleSubmit = () => {
+    const [video, setVideo] = useState({
+        title: 'Space Technology',
+        channel: 'NASA Official',
+        views: '4.5M',
+        time: '2 years ago',
+        verified: true
+    });
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(video);
     }
 
     const handleChange = (e) => {
-        console.log(e.target.value);
+        console.log(e.target.name, e.target.value);
+        setVideo({
+            ...video,
+            [e.target.name]: e.target.value
+        });
+        //console.log(video);
     }
 
     return (
         <>
             <form>
-                <input type="text" onChange={handleChange} placeholder='Video title' />
-                <input type="text" onChange={handleChange} placeholder='Channel Name' />
+                <input type="text" name='title' onChange={handleChange} placeholder='Video title' />
+                <input type="text" name='views' onChange={handleChange} placeholder='Channel Name' />
                 <button className='addVideoButton' onClick={handleSubmit}>Add Video</button>
             </form>
         </>
