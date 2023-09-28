5dc482822f388bafe961fad4c8ff57ce3a1fe621
developersview <pcslg1998@gmail.com>
2023-09-29 01:25:35 +0530

    Chapter 6: React Forms

--- react-masterclass/src/components/AddVideo.jsx
+++ react-masterclass/src/components/AddVideo.jsx
@@ -1,24 +1,26 @@
 import React, { useState } from 'react'
 import './AddVideo.css'
 
+const initialState = {
+    title: '',
+    channel: '',
+    views: '',
+    time: ''
+};
+
 const AddVideo = ({ onAdd }) => {
 
-    const [video, setVideo] = useState({
-        title: 'Space Technology',
-        channel: 'NASA Official',
-        views: '4.5M',
-        time: '2 years ago',
-        verified: true
-    });
+    const [video, setVideo] = useState(initialState);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         onAdd(video);
         console.log(video);
+        setVideo(initialState)
     }
 
     const handleChange = (e) => {
-        console.log(e.target.name, e.target.value);
+        //console.log(e.target.name, e.target.value);
         setVideo({
             ...video,
             [e.target.name]: e.target.value
@@ -29,8 +31,34 @@ const AddVideo = ({ onAdd }) => {
     return (
         <>
             <form>
-                <input type="text" name='title' onChange={handleChange} placeholder='Video title' />
-                <input type="text" name='views' onChange={handleChange} placeholder='Channel Name' />
+                <input
+                    type="text"
+                    name='channel'
+                    onChange={handleChange}
+                    placeholder='Video Channel'
+                    value={video.channel}
+                />
+                <input
+                    type="text"
+                    name='title'
+                    onChange={handleChange}
+                    placeholder='Video title'
+                    value={video.title}
+                />
+                <input
+                    type="text"
+                    name='views'
+                    onChange={handleChange}
+                    placeholder='Videos views'
+                    value={video.views}
+                />
+                <input
+                    type="text"
+                    name='time'
+                    onChange={handleChange}
+                    placeholder='Video time'
+                    value={video.time}
+                />
                 <button className='addVideoButton' onClick={handleSubmit}>Add Video</button>
             </form>
         </>
